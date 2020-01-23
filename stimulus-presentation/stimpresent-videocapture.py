dad1d7de6337d17a5d28448ca5bcde759f95e427
tonykylim <tony.ky.lim@gmail.com>
2020-01-23 14:50:19 -0500

    initial commit

---
+++ stimulus-presentation/stimpresent-videocapture.py
@@ -0,0 +1,89 @@
+import numpy, cv2, datetime, threading, time, csv
+from psychopy import visual, core, event, gui
+from psychopy.visual import ShapeStim
+
+experiment_types = ['blackloom', 'whiteloom']
+experiment_type = experiment_types[0]
+if experiment_type == 'blackloom':
+    background_colour = (1, 1, 1)
+    loom_colour = (-1, -1, -1)
+if experiment_type == 'whiteloom':
+    background_colour = (-1, -1, -1)
+    loom_colour = (1, 1, 1)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap.set(cv2.CAP_PROP_FOCUS, 35)
+cap.set(cv2.CAP_PROP_FPS, 30)
+while True:
+    ret, frame = cap.read()
+    if ret==False:
+        continue
+    break
+expInfo = {'Animal ID':'', 'timepoint':'', 'treatment':''}
+expInfo['exp_type'] = experiment_type
+expInfo['dateStr'] = str(datetime.date.today())
+dlg = gui.DlgFromDict(expInfo, fixed=['exp_type', 'dateStr'])
+if dlg.OK:
+    pass 
+else:
+    core.quit()
+capture_data = []
+def capture(trial_num):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(expInfo['Animal ID'] + '_'+ expInfo['timepoint'] + '_' + expInfo['treatment'] +'_trial' + str(trial_num)+'.avi',fourcc, 30, (640,480))
+    trial_dict = {}
+    trial_dict['trial num'] = trial_num
+    length = 0
+    frame_count = 0
+    start = time.time()
+    trial_dict['start'] = start
+    while(length < 15):
+        ret, frame = cap.read()
+        out.write(frame)
+        frame_count += 1
+        length = time.time() - start
+    trial_dict['frames'] = frame_count
+    trial_dict['duration'] = length
+    trial_dict['fps'] = frame_count/length
+    capture_data.append(trial_dict)
+mywin = visual.Window([800, 600], monitor="projector", screen=1, fullscr=True, units="pix", pos=None, color=background_colour, colorSpace='rgb')
+stim_time =[]
+def stimulus():
+    myradius = 1.0
+    mysize = 1.0
+    mycircle = visual.Circle(win = mywin, radius = myradius, edges = 128, color=loom_colour, size = mysize )
+    mycircle.draw()
+    mywin.flip()
+    core.wait(2)
+    for i in range(250):
+        mysize += 0.1
+        mycircle.size = mysize
+        mycircle.draw()
+        mywin.flip()
+    core.wait(2)
+    stim_time.append(time.time())
+    for i in range(50):
+        mycircle.setSize(1.1, '*')
+        mycircle.draw()
+        mywin.flip()
+    core.wait(0.5)
+    mycircle.size = 1
+    mycircle.draw()
+    mywin.flip()
+for trial in range(10):
+    mythread = threading.Thread(target=capture,args=[trial])
+    mythread.start()  
+    stimulus()
+    mythread.join()
+    core.wait(1)
+cap.release()
+cv2.destroyAllWindows()
+for x in range(10):
+    capture_data[x].update( {'stim time': stim_time[x] } )
+keys = capture_data[0].keys()
+with open(expInfo['Animal ID'] + '_'+ expInfo['timepoint'] + '_' + expInfo['treatment'] + '_' +  expInfo['exp_type'] + '_timings.csv', 'w') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(capture_data)
+mywin.close()
+core.quit()
