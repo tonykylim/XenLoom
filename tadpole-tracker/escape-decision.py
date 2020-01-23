f04b7c6972f33221a48b9c22b5e6940439c91e8a
tonykylim <tony.ky.lim@gmail.com>
2020-01-23 18:05:01 -0500

    added escape decision script

---
+++ tadpole-tracker/escape-decision.py
@@ -0,0 +1,108 @@
+import glob, random, cv2, csv, os
+from datetime import datetime
+from tkinter import *
+
+def user_yes():
+    global replay
+    replay = False
+    input = 1
+    now = datetime.now()
+    data_to_save = [ animalID, timepoint, treatment, trial[-1:], input, now.strftime("%y/%m/%d %H:%M") ]
+    with open('response_to_loom.csv', 'a', newline='') as datafile:
+        datafilewriter = csv.writer(datafile)
+        datafilewriter.writerow( data_to_save )
+        print("Trial data saved to response_to_loom.csv")
+    win.destroy()
+
+def user_no():
+    global replay
+    replay = False
+    input = 0
+    now = datetime.now()
+    data_to_save = [ animalID, timepoint, treatment, trial[-1:], input, now.strftime("%y/%m/%d %H:%M") ]
+    with open('response_to_loom.csv', 'a', newline='') as datafile:
+        datafilewriter = csv.writer(datafile)
+        datafilewriter.writerow( data_to_save )
+        print("Trial data saved to response_to_loom.csv")
+    win.destroy()
+    
+def user_unsure():
+    global replay
+    replay = False
+    input = ''
+    now = datetime.now()
+    data_to_save = [ animalID, timepoint, treatment, trial[-1:], input, now.strftime("%y/%m/%d %H:%M") ]
+    with open('response_to_loom.csv', 'a', newline='') as datafile:
+        datafilewriter = csv.writer(datafile)
+        datafilewriter.writerow( data_to_save )
+        print("Trial data saved to response_to_loom.csv")
+    win.destroy()
+    
+def user_replay():
+    global replay
+    replay = True
+    win.destroy()
+
+if os.path.exists('response_to_loom.csv') == False:
+        with open ('response_to_loom.csv', 'w', newline='') as datafileinit:
+            datafileinitwriter = csv.writer(datafileinit)
+            datafileinitwriter.writerow( [ 'Animal ID', 'Timepoint', 'Treatment', 'Trial #', 'Response', 'Timestamp' ] )
+
+video_file_list = glob.glob("*.avi")
+random.shuffle(video_file_list)
+num_videos = len(video_file_list)
+video_num_counter = 0
+for video_file in video_file_list:
+    video_num_counter += 1
+    print('Playing video ' + str(video_num_counter) + ' of ' + str(num_videos))
+    video = cv2.VideoCapture(video_file)
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    filename = video_file[:-4]
+    animalID, timepoint, treatment, trial = filename.split("_")
+    trial_num = int(trial[-1:])
+    capture_data = []
+    with open(animalID + '_' + timepoint + '_' + treatment + '_blackloom_timings.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            capture_data.append(row)
+    stim_start = float(capture_data[trial_num]['stim time'])
+    trial_start = float(capture_data[trial_num]['start'])
+    stim_time = stim_start - trial_start
+    videofps = float(capture_data[trial_num]['fps'])
+    stim_frame = int( stim_time * videofps )
+    buffer_2 = 2 * videofps
+    replay = True
+    while replay == True:
+        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        frame_num = 0
+        while True:
+            ok, frame = video.read()
+            if not ok:
+                break
+            frame_num += 1
+            if frame_num > stim_frame and frame_num < stim_frame + buffer_2:
+                cv2.putText(frame, "LOOMING", (frame.shape[1] - 180, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 4)
+            cv2.imshow("Video", frame)
+            k = cv2.waitKey(10) & 0xff
+            if k == 27 : break
+        win = Tk()
+        f = Frame(win)
+        b1 = Button(f, text="Yes", command=user_yes)
+        b2 = Button(f, text="No", command=user_no)
+        b3 = Button(f, text="Unsure", command=user_unsure)
+        b4 = Button(f, text="Replay", command=user_replay)
+        b1.pack(side=LEFT)
+        b2.pack(side=LEFT)
+        b3.pack(side=LEFT)
+        b4.pack(side=LEFT)
+        l = Label(win, text="Did the tadpole respond to the looming stimuli?")
+        l.pack()
+        f.pack()
+        f.mainloop()
