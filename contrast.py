ebda3283bcb5673815ef463fd98ece6f8a485676
tonykylim <tony.ky.lim@gmail.com>
2020-02-03 17:55:15 -0500

    fixed exit issue

--- contrast.py
+++ contrast.py
@@ -1,7 +1,10 @@
-from psychopy import visual
+from psychopy import visual, event
+
 
 contrast = 1
 
 background_colour = (contrast, contrast, contrast)
 mywin = visual.Window([800, 600], monitor="projector", screen=1, fullscr=True, units="pix", pos=None, color=background_colour, colorSpace='rgb')
-input ("Press enter to continue")
+
+while event.waitKeys() == False:
+    continue
