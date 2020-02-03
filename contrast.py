ede69e9b536ed2821690ed6393827abde4b2c8f6
tonykylim <tony.ky.lim@gmail.com>
2020-02-03 16:30:12 -0500

    contrast

---
+++ contrast.py
@@ -0,0 +1,7 @@
+from psychopy import visual
+
+contrast = 1
+
+background_colour = (contrast, contrast, contrast)
+mywin = visual.Window([800, 600], monitor="projector", screen=1, fullscr=True, units="pix", pos=None, color=background_colour, colorSpace='rgb')
+input ("Press enter to continue")
