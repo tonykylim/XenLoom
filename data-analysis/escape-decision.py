84cfeeeb5c34271e51f0afb66086667b0634f902
Tony Lim <tony.ky.lim@gmail.com>
2020-03-12 05:42:10 -0400

    fixing sorting

--- data-analysis/escape-decision.py
+++ data-analysis/escape-decision.py
@@ -121,6 +121,6 @@ with open('response_to_loom.csv', mode='rt', newline='') as data, open('response
     writer = csv.writer(sorted_data, delimiter=',')
     reader = csv.reader(data, delimiter=',')
     writer.writerow(next(reader))
-    data = sorted(reader, key=lambda row: (row[0], int(row[3])))        
+    data = sorted(reader, key=lambda row: ((row[0], row[2], int(row[3])))
     for row in data:
         writer.writerow(row)
